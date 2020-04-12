ae34f3d71d27c3948cfc3bbcfe559543873ad302
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-04-12 18:26:01 +0300

    complete-note-form

---
+++ doc-notes/src/components/edit-form.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import { Link } from "react-rooter-dom";
+import styled from "styled-components";
+
+const EditFormDiv = styled.div`
+  form {
+    ${"" /* border: 1px solid blue; */}
+    background-color: #F3F3F3;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .input {
+      margin: 10px;
+      padding: 10px;
+      color: gray;
+    }
+    #title {
+      width: 30%;
+    }
+    #body {
+      height: 400px;
+      text-emphasis: wrap;
+      max-width: 90%;
+      font-family: "Roberto";
+      line-height: 20px;
+    }
+    .menu-item {
+      ${"" /* border: 1px solid red; */}
+      width: 30%;
+      padding: 15px;
+      margin: 5px;
+      text-align: center;
+      text-decoration: none;
+      color: white;
+      background-color: #2ac0c4;
+      font-weight: bold;
+      border: 1px solid lightgray;
+    }
+  }
+`;
+export default class EditForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.note.title,
+      body: this.props.note.body,
+      id: this.props.note.id,
+      count: props.count,
+    };
+  }
+  sentToApp = (e) => {
+    console.log(this.props);
+    if (this.props.button === "Create") {
+      this.props.newNote(this.state);
+    } else {
+      this.props.editNote(this.state);
+    }
+    this.setState({
+      title: "",
+      body: "",
+    });
+  };
+  inputHandler = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  render() {
+    return (
+      <EditFormDiv>
+        <form onSubmit={this.sendToApp}>
+          <input
+            className="input"
+            id="title"
+            onChange={this.inputHandler}
+            name="title"
+            value={this.state.title}
+            placeholder="Note Title"
+          >
+            {this.value}
+          </input>
+          <textarea
+            className="input"
+            id="body"
+            name="body"
+            onChange={this.inputHandler}
+            value={this.state.body}
+            placeholder="Note Content"
+          >
+            {this.value}
+          </textarea>
+          <Link
+            className="menu-item"
+            onClick={this.sendToApp}
+            to={`/all-notes/${this.state.id}`}
+          >
+            {this.props.button}
+          </Link>
+        </form>
+      </EditFormDiv>
+    );
+  }
+}
---
+++ doc-notes/src/components/edit-note.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import EditForm from "./edit-form.js";
+
+const EditNoteDiv = styled.div`
+  ${"" /* border: 1px solid green; */}
+  display: flex;
+  flex-direction: column;
+  background-color: #f3f3f3;
+  padding: 25px;
+  background-color: white;
+  height: 100vh;
+  .links {
+    ${"" /* border: 1px solid blue; */}
+    padding: 15px;
+    text-align: right;
+    .link {
+      margin: 8px;
+      color: gray;
+    }
+  }
+  h4 {
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+`;
+export default class EditNote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      note: props.note,
+      count: props.count,
+    };
+  }
+  sendToApp = () => {};
+  render() {
+    return (
+      <EditNoteDiv>
+        <div className="links">
+          <Link className="link" to={`/all-notes/${this.state.note.id}`}>
+            back
+          </Link>
+        </div>
+        <h4>Edit Note:</h4>
+        <EditForm
+          button="Save"
+          count={this.state.count}
+          editNote={this.props.editNote}
+          note={this.state.note}
+        ></EditForm>
+      </EditNoteDiv>
+    );
+  }
+}
