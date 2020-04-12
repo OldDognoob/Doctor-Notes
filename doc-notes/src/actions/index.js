91f6e7a5bc09020beb3a527499308406ed3526b3
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-04-12 13:50:22 +0300

    complete-action-index

--- doc-notes/src/actions/index.js
+++ doc-notes/src/actions/index.js
@@ -1,7 +1,20 @@
 //Design the type of actions the state may suffer
-import axios from 'axios';
+import axios from "axios";
 
-export const FETCHING_NOTES='FETCHING_NOTES';
-export const NOTES_RETRIEVED='NOTES_RETRIEVED';
-export const ERROR='ERROR';
+export const FETCHING_NOTES = "FETCHING_NOTES";
+export const NOTES_RETRIEVED = "NOTES_RETRIEVED";
+export const ERROR = "ERROR";
 
+export const getNotes = () => {
+  return function (dispatch) {
+    dispatch({ type: FETCHING_NOTES });
+    axios
+      .get("https://doc-notes.herokuapp.com/note/get/all")
+      .then((res) => {
+        dispatch({ type: NOTES_RETRIEVED, payload: res.data });
+      })
+      .catch((error) => {
+        dispatch({ type: ERROR, payload: error });
+      });
+  };
+};
