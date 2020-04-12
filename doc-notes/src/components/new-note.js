601b3e26a5c80b773676b39ee982e663456e1db5
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-04-12 18:30:24 +0300

    complete-new-note

---
+++ doc-notes/src/components/new-note.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import NewForm from "./new-form.js";
+import styled from "styled-components";
+
+const NewNoteDiv = styled.div`
+  ${"" /* border: 1px solid green; */}
+  display: flex;
+  flex-direction: column;
+  background-color: #f3f3f3;
+  padding: 25px;
+  height: 100vh;
+  h4 {
+    font-weight: bold;
+    margin: 0 0 10px 10px;
+  }
+`;
+export default class NewNote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: props.notes,
+      count: props.count,
+    };
+  }
+  render() {
+    console.log(this.props.count);
+    return (
+      <NewNoteDiv>
+        <h4>Create a new Note:</h4>
+        <NewForm
+          button="Create"
+          count={this.props.count}
+          newNote={this.props.newNote}
+        ></NewForm>
+      </NewNoteDiv>
+    );
+  }
+}
